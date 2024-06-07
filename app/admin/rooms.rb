a09864cc37a9a3c1cfef04d4e82f98526d8720b9
Adarsh Mishra <aadimishra24@gmail.com>
2024-06-08 03:34:14 +0530

    rooms using ad

---
+++ app/admin/rooms.rb
@@ -0,0 +1,47 @@
+ActiveAdmin.register Room do
+    permit_params :name
+  
+    # Scope to filter out private rooms
+    scope :all, default: true do |rooms|
+      rooms.where(is_private: false)
+    end
+  
+    index do
+      selectable_column
+      column :name
+      column :created_at
+      column :updated_at
+      actions
+    end
+  
+    filter :name
+    filter :created_at
+    filter :updated_at
+  
+    form do |f|
+      f.inputs do
+        f.input :name
+      end
+      f.actions
+    end
+  
+    show do
+      attributes_table do
+        row :id
+        row :name
+        row :created_at
+        row :updated_at
+      end
+  
+      panel "Messages" do
+        table_for room.messages do
+          column :id
+          column :content
+          column :user
+          column :created_at
+          column :updated_at
+        end
+      end
+    end
+  end
+  
