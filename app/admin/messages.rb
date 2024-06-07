02c0300a094f93b70a2c0b50723dc50872fb9a7c
Adarsh Mishra <aadimishra24@gmail.com>
2024-06-08 01:31:46 +0530

    added active admin functionality'

---
+++ app/admin/messages.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register_page "Messages" do
+    menu priority: 2, label: "All Messages"
+  
+    content title: "All Messages" do
+      panel "Messages" do
+        table_for Message.unscoped.order(created_at: :desc) do
+          column :user do |message|
+            link_to message.user.username, admin_user_path(message.user)
+          end
+          column :content
+          column :created_at
+          column :room
+          column :action do |message|
+            if message.deleted_at?
+              status_tag "Deleted at #{message.deleted_at.strftime('%Y-%m-%d %H:%M:%S')}", class: 'deleted'
+            else
+              status_tag 'Active', class: 'active'
+            end
+          end
+        end
+      end
+    end
+  end
+  
