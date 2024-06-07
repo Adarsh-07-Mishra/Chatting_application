ae7f280694aec8bd494b0c017f02949ffa692437
Adarsh Mishra <aadimishra24@gmail.com>
2024-06-07 19:39:11 +0530

    added javascript for scroll bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
    validates :username, presence: true, length: { minimum: 1 }
   scope :all_except, ->(user) { where.not(id: user) }
   after_create_commit { broadcast_append_to 'users' }
+
+  def self.ransackable_associations(auth_object = nil)
+    ["messages"]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "password_digest", "updated_at", "username"]
+  end
 end
