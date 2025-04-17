03716e29eb881ad3ce20bb465ed7edba373b1df1
Adarsh Mishra <aadimishra24@gmail.com>
2025-04-18 00:18:42 +0530

    corrected render.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,20 @@
 # exit on error
 set -o errexit
 
+# Force Ruby platform for Nokogiri
+bundle config set force_ruby_platform true
+
+# Install dependencies
 bundle install
+
+# Precompile assets
 bundle exec rails assets:precompile
+
+# Clean assets
 bundle exec rails assets:clean
+
+# Run database migrations
 bundle exec rake db:migrate
+
+# Seed the database
 bundle exec rails db:seed
