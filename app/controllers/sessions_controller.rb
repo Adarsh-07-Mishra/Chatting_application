390ae1e34ddf413a5361032ffcac6e6f1c7f5b23
Adarsh-07-Mishra <aadimishra24@gmail.com>
2025-04-24 14:06:38 +0530

    add rubocop file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,6 @@
 
 class SessionsController < ApplicationController
   def create
-
-
-    
     @user = User.find_by(username: params[:session][:username])
 
     if @user&.authenticate(params[:session][:password])
